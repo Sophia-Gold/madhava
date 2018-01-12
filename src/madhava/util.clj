afe936d28ae51519e3970bef2f7f60f1e7ea27d3
Sophia Gold <sophiagoldnyc@gmail.com>
2018-01-12 01:43:58 -0500

    grevlex comparator

--- src/madhava/util.clj
+++ src/madhava/util.clj
@@ -1,6 +1,6 @@
 (ns madhava.util
   (:require [com.rpl.specter :refer :all]
-            [clojure.data.int-map :as i]))
+            [clojure.data.avl :as avl]))
 
 (defn add-dim [poly]
   ;; projects into next higher dimension by appending zero to tuples of variables
@@ -8,3 +8,20 @@
 
 (defn denull [poly]
   (setval [MAP-VALS #(= % 0)] NONE poly))  
+
+(defn grevlex [poly1 poly2]
+  (let [grade1 (reduce +' poly1)
+        grade2 (reduce +' poly2)
+        comp (- grade1 grade2)]
+    (if (not= 0 comp)
+      comp
+      (loop [poly1 poly1
+             poly2 poly2]
+        (let [grade1 (first poly1)
+              grade2 (first poly2)
+              comp (- grade1 grade2)]
+          (cond
+            (empty? poly1) 0
+            (not= 0 comp) comp
+            :else (recur (next poly1)
+                         (next poly2))))))))
