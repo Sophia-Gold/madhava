f497ffd5c191519de947b99d21ea167b97ea91fd
Sophia Gold <sophiagoldnyc@gmail.com>
2018-01-14 03:08:57 -0500

    grevlex comparator

--- src/madhava/util.clj
+++ src/madhava/util.clj
@@ -26,3 +26,35 @@
         (zero? n) i
         (> n idx) (recur (quot i 10) (dec n))
         :else (recur (mod i 10) (dec n))))))
+
+(defn grevlex [term1 term2]
+  (letfn [(grade [i]
+            (let [dims (dims i)]
+              (loop [count dims
+                     result 0]
+                (if (zero? count)
+                  result
+                  (recur (inc count) (+ result
+                                        (int-nth i count)))))))]                      
+    (let [grade1 (grade term1)
+          grade2 (grade term2)
+          comp (- grade2 grade1)] ;; total degree
+      (if (not= 0 comp)
+        comp
+        (loop [term1 term1
+               term2 term2]
+          (let [dims1 (dims term1)
+                dims2 (dims term2)]
+            (cond
+              (and
+               (zero? dims1)
+               (zero? dims2)) 0
+              (zero? dims1)  term1
+              (zero? dims2)  (- term2)
+              :else (let [grade1 (int-nth term1 (dec dims1))
+                          grade2 (int-nth term2 (dec dims2))
+                          comp (- grade1 grade2)] ;; differs from grlex because terms are flipped from above
+                      (if (not= 0 comp)
+                        comp
+                        (recur (quot term1 10)
+                               (quot term2 10)))))))))))
