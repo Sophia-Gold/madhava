ae7849c490ee54eecd33079ef695ef310ca78659
Sophia Gold <sophiagoldnyc@gmail.com>
2018-01-25 22:40:57 -0500

    patched chain rule

--- src/madhava/comp.clj
+++ src/madhava/comp.clj
@@ -46,39 +46,28 @@
                             (+ y (* x %1))))) ;; substitute values in rest args for variables in f
             rest)
        (reduce *'))) ;; multiply result of evaluating each variable
-       
+
+(defn chain1 [f g idx]
+  ;; faster for univariate functions
+  (let [i (dec idx)]
+    (-> f
+        (grad)
+        (nth i)
+        (compose g idx)
+        (mul (nth (grad g) i)))))
+
 (defn chain
-  ;; transducer form of compose
+  ;; transducer
   ([f]  ;; completion
    (fn
      ([] f)
      ([g] (chain f g))
      ([g & more] (chain f g more))))
-  ([f g]
-   (let [vars (count (ffirst f))
-         grad-f (grad f)
-         grad-g (grad g)
-         f-g (map #(compose f g (inc %)) (range vars))]
-     (map add
-          (map-indexed #(mul (nth grad-f %1) %2) f-g)
-          (map-indexed #(mul (nth grad-g %1) %2) f-g))))
+  ([f g] 
+   (->> f
+        (grad)
+        (map-indexed #(compose %2 g (inc %1)))
+        (map mul (grad g))
+        (reduce add)))
   ([f g & more]
    (reduce chain (chain f g) more)))
-
-(defn rchain
-  ;; transducer form of compose
-  ([f]  ;; completion
-   (fn
-     ([] f)
-     ([f g] (rchain f g))
-     ([f g & more] (apply rchain f g more))))
-  ([f g]
-   (let [vars (count (ffirst f))
-         grad-f (grad f)
-         grad-g (grad g)
-         g-f (map #(compose g f (inc %)) (range vars))]  ;; transpose matroid
-     (map add                                                  
-          (map-indexed #(mul (nth grad-f %1) %2) g-f)
-          (map-indexed #(mul (nth grad-g %1) %2) g-f))))
-  ([f g & more]
-   (reduce rchain (rseq (conj more (rchain f g))))))  ;; reverse mode AD is just reversing a vector!
