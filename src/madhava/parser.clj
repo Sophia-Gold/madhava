318c4f5ee3e7a0d2975222b39518fc5d9a88acbf
Sophia Gold <sophiagoldnyc@gmail.com>
2018-02-18 06:57:27 -0500

    ssa getters and setters

--- src/madhava/parser.clj
+++ src/madhava/parser.clj
@@ -6,11 +6,25 @@
 (defmacro ssa [f]
   `(-> ~f
        ast
-       second
        parse-to-state-machine
        second
        :blocks
        vals
        first))
+
+(defn ret [f]
+  (->> f
+       (filter #(= (class %) clojure.core.async.impl.ioc_macros.Return))
+       (first)
+       :value))
+
+(defn exprs [f]
+  (filter #(= (class %) clojure.core.async.impl.ioc_macros.Call) f))
+
+(defn expr-iter [f next]
+  (->> f
+       (filter #(= (:id %) next))
+       first
+       :refs))
 
 (def t (ssa example))
