6dbcf50ee07ced980b51c1626b21f4f4106e5811
Sophia Gold <sophiagoldnyc@gmail.com>
2018-03-13 03:57:08 -0400

    state machine -> control flow graph

--- src/madhava/parser.clj
+++ src/madhava/parser.clj
@@ -1,7 +1,20 @@
 (ns madhava.parser
   (:require [madhava.ast :refer [ast example]]
+            [madhava.util :refer :all]
             [clojure.core.async.impl.ioc-macros :refer [parse-to-state-machine]]
-            [clojure.pprint :refer [pprint]]))
+            [clojure.pprint :refer [pprint]]
+            [clojure.data.avl :refer [sorted-map-by]]
+            [clj-tuple :refer [vector]])
+  (:refer-clojure :exclude [vector sorted-map-by]))
+
+(defn ** [base exp]
+  (long (Math/pow base exp)))
+
+(def dict {'clojure.core/+ 'add
+           'clojure.core/- 'sub
+           'clojure.core/* 'add
+           'clojure.core// 'div
+           'madhava.parser/** 'pow})
 
 (defmacro ssa [f]
   `(-> ~f
@@ -12,19 +25,61 @@
        vals
        first))
 
-(defn ret [f]
-  (->> f
-       (filter #(= (class %) clojure.core.async.impl.ioc_macros.Return))
-       (first)
-       :value))
+(defmacro ssa2 [f] 
+  `(-> ~f
+       quote
+       clojure.repl/source-fn
+       read-string
+       nnext 
+       parse-to-state-machine
+       second
+       :blocks
+       vals
+       first))
 
-(defn exprs [f]
-  (filter #(= (class %) clojure.core.async.impl.ioc_macros.Call) f))
-
-(defn expr-iter [f next]
-  (->> f
-       (filter #(= (:id %) next))
+(defn get-block [id asm]
+  (->> asm
+       (filterv #(= (:id %) id))
        first
        :refs))
 
-(def t (ssa example))
+(defn args [asm]
+  (->> asm
+       first
+       :refs
+       next
+       (into (vector))))
+
+(defn ret [asm]
+  (-> asm
+      last
+      :value
+      (get-block asm)))
+
+(defn trace-control-flow [asm]
+  (let [args (args asm)]
+    (letfn [(trace [block]
+              (let [op (keyword (name (first block))) 
+                    children (mapv #(if (and (symbol? %)
+                                             (empty? (filter (partial = %) args)))
+                                      (trace (get-block % asm))
+                                      %)
+                                   (next block))]
+                {op children}))]
+      (trace (ret asm)))))
+
+;; (defn trace-control-flow2 [f]
+;;   (let [asm (ssa f)
+;;         args (args asm)]
+;;     (letfn [(trace [block]
+;;               (let [op (keyword (name (first block))) 
+;;                     children (mapv #(if (and (symbol? %)
+;;                                              (empty? (filter (partial = %) args)))
+;;                                       (trace (get-block % asm))
+;;                                       %)
+;;                                    (next block))]
+;;                 {op children}))]
+;;       (trace (ret asm)))))
+
+(def t1 (ssa example))
+(def t2 (ssa2 example))
